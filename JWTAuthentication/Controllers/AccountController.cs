6a01b1cf3204f05f1971c4f62db0e4e74965d6a2
= <=>
2022-08-13 00:33:21 +0500

    first commit

---
+++ JWTAuthentication/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using JWTAuthentication.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuthentication.Controllers
+{
+	[Authorize]
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AccountController : ControllerBase
+	{
+		private readonly IJWTAuthenticationManager AuthenticationManager;
+
+		public AccountController(IJWTAuthenticationManager authenticationManager)
+		{
+			AuthenticationManager = authenticationManager;
+		}
+
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "New Jersey", "Ohio", "New york" };
+		}
+
+		[AllowAnonymous]
+		[HttpPost("authenticate")]
+		public IActionResult Authenticate([FromBody] UserCred userCred)
+		{
+			var token = AuthenticationManager.Authenticate(userCred.UserName, userCred.Password);
+			if (token is null)
+			{
+				return Unauthorized();
+			}
+			return Ok(token);
+		}
+	}
+}
---
+++ JWTAuthentication/JWTAuthenticationManager.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthentication
+{
+	public class JWTAuthenticationManager : IJWTAuthenticationManager
+	{
+		private readonly IDictionary<string, string> users = new Dictionary<string, string> { { "user1", "pass1" }, { "user2", "pass2" } };
+		private readonly string key;
+
+		public JWTAuthenticationManager(string key)
+		{
+			this.key = key;
+		}
+
+		public string Authenticate(string userName, string password)
+		{
+			if (!users.Any(x => x.Key == userName && x.Value == password))
+			{
+				return null;
+			}
+
+			var tokenHandler = new JwtSecurityTokenHandler();
+			var tokenKey = Encoding.ASCII.GetBytes(key);
+
+			var tokenDescriptor = new SecurityTokenDescriptor
+			{
+				Subject = new ClaimsIdentity(new Claim[] {
+					new Claim(ClaimTypes.Name, userName)
+				}),
+				Expires = DateTime.UtcNow.AddMinutes(1),
+				SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey), 
+				SecurityAlgorithms.HmacSha256Signature)
+			};
+
+			var token = tokenHandler.CreateToken(tokenDescriptor);
+			return tokenHandler.WriteToken(token);
+		}
+	}
+}
